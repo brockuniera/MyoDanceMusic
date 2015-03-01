09664764ad4a38d6ccf8261d2aaa1840c2c286fb
Brandon Pearson <brandnpearson@gmail.com>
2015-03-01 07:19:27 -0500

    Converted spaces to tabs

--- include/CircularBuffer.hpp
+++ include/CircularBuffer.hpp
@@ -7,36 +7,36 @@
 class CircularBuffer {
 public:
 
-  // Creates a buffer with 'slots' slots.
-  explicit CircularBuffer(int slots);
-  // Destructor.
-  ~CircularBuffer();
-  // Writes 'value' to the next available slot. It may overwrite
-  // values that were not yet read out of the buffer.
-  void write(int value);
-  // Returns the next value available for reading, in the order they
-  // were written, and marks slot as read. If the buffer is empty returns -1.
-  int read();
-  // Removes all the elements from the buffer.
-  void clear();
-  // Computes the average
-  float rollingAverage();
-  // Computes the derivative
-  float findDerivative();
+	// Creates a buffer with 'slots' slots.
+	explicit CircularBuffer(int slots);
+	// Destructor.
+	~CircularBuffer();
+	// Writes 'value' to the next available slot. It may overwrite
+	// values that were not yet read out of the buffer.
+	void write(int value);
+	// Returns the next value available for reading, in the order they
+	// were written, and marks slot as read. If the buffer is empty returns -1.
+	int read();
+	// Removes all the elements from the buffer.
+	void clear();
+	// Computes the average
+	float rollingAverage();
+	// Computes the derivative
+	float findDerivative();
 
 private:
-  //array of integers
-  int* data_;
-  // the size of the buffer
-  int  num_of_slots_;
-  //index to read the next integer from buffer
-  int  read_index_;
-  //index to write a new integer to buffer
-  int  write_index_;
+	//array of integers
+	int* data_;
+	// the size of the buffer
+	int	num_of_slots_;
+	//index to read the next integer from buffer
+	int	read_index_;
+	//index to write a new integer to buffer
+	int	write_index_;
 
-  // Non-copyable, non-assignable.
-  CircularBuffer(CircularBuffer&);
-  CircularBuffer& operator=(const CircularBuffer&);
+	// Non-copyable, non-assignable.
+	CircularBuffer(CircularBuffer&);
+	CircularBuffer& operator=(const CircularBuffer&);
 };
 
-#endif  // _CIRCULAR_BUFFER_H_
+#endif	// _CIRCULAR_BUFFER_H_
--- src/CircularBuffer.cpp
+++ src/CircularBuffer.cpp
@@ -2,49 +2,49 @@
 #include "CircularBuffer.hpp"
 
 CircularBuffer::CircularBuffer(int slots) {
-  if (slots <= 0) {
-    num_of_slots_ = 10; /*pre-assigned value */
-  } else {
-      num_of_slots_ = slots;
-  }
-  clear();
+	if (slots <= 0) {
+	num_of_slots_ = 10; /*pre-assigned value */
+	} else {
+		num_of_slots_ = slots;
+	}
+	clear();
 }
 
 CircularBuffer::~CircularBuffer() {
-  delete[] data_;
+	delete[] data_;
 }
 
 void CircularBuffer::write(int value) {
-  data_[write_index_] = value;
-  if (read_index_ == -1) {
-    //if buffer is empty, set the read index to the 
+	data_[write_index_] = value;
+	if (read_index_ == -1) {
+	//if buffer is empty, set the read index to the 
  //current write index. because that will be the first 
  //slot to be read later.
-    read_index_ = write_index_; 
-  }
-  write_index_ = (write_index_ + 1) % num_of_slots_; 
+	read_index_ = write_index_; 
+	}
+	write_index_ = (write_index_ + 1) % num_of_slots_; 
 }
 
 int CircularBuffer::read() {
-  if (read_index_ == -1) {
-    // buffer empty
-    return -1;
-  }
-  int ret_val = data_[read_index_];
-  read_index_ = (read_index_ + 1) % num_of_slots_;
-  if (read_index_ == write_index_) {
-    /*all available data is read, now buffer is empty*/
-    read_index_ = -1;
-  }
+	if (read_index_ == -1) {
+	// buffer empty
+	return -1;
+	}
+	int ret_val = data_[read_index_];
+	read_index_ = (read_index_ + 1) % num_of_slots_;
+	if (read_index_ == write_index_) {
+	/*all available data is read, now buffer is empty*/
+	read_index_ = -1;
+	}
 
-  return ret_val;  
+	return ret_val;	
 }
 
 void CircularBuffer::clear() {
-  read_index_ = -1; /* buffer empty */
-  write_index_ = 0; /* first time writing to that buffer*/
-  delete [] data_;
-  data_ = new int[num_of_slots_]; /* allocate space for buffer */
+	read_index_ = -1; /* buffer empty */
+	write_index_ = 0; /* first time writing to that buffer*/
+	delete [] data_;
+	data_ = new int[num_of_slots_]; /* allocate space for buffer */
 }
 
 float CircularBuffer::rollingAverage() {
@@ -57,11 +57,11 @@ float CircularBuffer::rollingAverage() {
 }
 
 float CircularBuffer::findDerivative() {
-  float totalDerivative = 0.0;
-  for (int i = 0; i < num_of_slots_; i++) {
-    totalDerivative += read()/read();
-  }
-
-  return (totalDerivative/(num_of_slots_ - 1));
+	float totalDerivative = 0.0;
+	for (int i = 0; i < num_of_slots_; i++) {
+		totalDerivative += read()/read();
+	}
+	
+	return (totalDerivative/(num_of_slots_ - 1));
 }
 
