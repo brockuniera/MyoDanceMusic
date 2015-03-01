8e90873515b8a8d8d69138855ce17c0823346968
Khanguy <khanguy101@gmail.com>
2015-03-01 02:25:55 -0500

    Merge RPP to MidiSide
    Main changes

--- src/main.cpp
+++ src/main.cpp
@@ -8,10 +8,6 @@ int main(){
 	MidiPlayer& mp = MidiPlayer::getInstance();
 	sleep(1);
 
-	while(1){
-		//should play a 16th note at 120bpm
-		mp.playNote(67, 0, 1);
-		mp.stopNotes();
 		usleep(microsPer16th);
 	}
 }
